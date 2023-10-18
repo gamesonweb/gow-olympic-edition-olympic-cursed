78c5cb655a2ba5e1e445fbb4c5a6da0b116e9294
Nayarjay <74157421+Nayarjay@users.noreply.github.com>
2023-10-18 11:26:29 +0200

    create nodejs structure

---
+++ game/devCamera.js
@@ -0,0 +1,3 @@
+// Configurez une caméra
+var devcamera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0, 5, -10), scene);
+camera.setTarget(BABYLON.Vector3.Zero());
--- server.js
+++ server.js
@@ -1,21 +1,124 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+"use strict"
 
-const server = http.createServer((req, res) => {
-    // Traitez ici les requêtes HTTP
-    const filePath = path.join(__dirname, req.url);
-    const fileStream = fs.createReadStream(filePath);
 
-    fileStream.on('error', () => {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('Not Found');
-    });
 
-    fileStream.pipe(res);
+//cookie
+const cookieSession = require('cookie-session');
+//var model = require('./model');
+
+
+
+var express = require('express');
+var mustache = require('mustache-express');
+
+var app = express();
+// DÃ©finition du dossier pour les fichiers statiques (images, CSS, etc.)
+
+
+
+// parse form arguments in POST requests
+const bodyParser = require('body-parser');
+const { rawListeners, title } = require('process');
+const { Console } = require('console');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.engine('html', mustache());
+app.set('view engine', 'html');
+app.set('views', './view');
+
+app.use(express.static('public'));
+
+app.use(express.static('./view/css'));
+app.use(express.static('./view/img'));
+app.use(express.static('game'));
+
+
+app.use(cookieSession({
+  secret: 'mot-de-passe-du-cookie',
+}));
+app.use(authenticated);
+//app.use(change_header);
+
+function is_authenticated(req, res, next) {
+ 
+  if (!req.session.userid) {
+   // res.redirect('/not_authenticated')
+    return res.redirect('/login')
+    
+  }
+  
+  next();
+}
+function authenticated(req, res, next) {
+  if(req.session.userid && req.session){
+    res.locals.authenticated =true;
+    res.locals.name = req.session.name;
+   
+  }else{
+    // la session n'est pas valide
+    res.locals.authenticated = false;
+  }
+ 
+  next();
+}
+
+
+
+/**** Routes pour voir les pages du site ****/
+
+/* Retourne une page principale avec le nombre de recettes */
+app.get('/', (req, res) => {
+
+
+    res.render('index.html');
+
 });
 
-const port = 3000;
-server.listen(port, () => {
-    console.log(`Serveur en cours d'exécution sur le port ${port}`);
+
+
+
+app.get('/Menu', (req, res) => {
+
+
+  res.render('gamedev.html');
+
 });
+
+
+app.get('/contact', (req, res) => {
+
+
+  res.render('contact.html');
+
+});
+
+app.get('/experience', (req, res) => {
+
+
+  res.render('experience.html');
+
+});
+
+
+app.get('/portfolio', (req, res) => {
+
+
+  res.render('portfolio.html');
+
+});
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+app.listen(3000, () => console.log('listening on http://localhost:3000'));
+
