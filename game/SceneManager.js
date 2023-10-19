7376a4a9602ff59d7634f7a5977f35f8d5d68e5a
Nayarjay <74157421+Nayarjay@users.noreply.github.com>
2023-10-19 09:48:54 +0200

    deuxième scène

--- game/SceneManager.js
+++ game/SceneManager.js
@@ -1,8 +1,10 @@
 // Créez une scène
 import *  as level1 from './scene1.js'; // Remplacez './fichier1' par le chemin vers votre fichier d'origine
+import *  as level2 from './scene2.js';
 
 function Main(){
-    level1.launch();
+    //level1.launch();
+    level2.launch();
 }
 
 
---
+++ game/scene2.js
@@ -0,0 +1,38 @@
+var canvas = document.getElementById("renderCanvas");
+var engine = new BABYLON.Engine(canvas, true);
+var scene = new BABYLON.Scene(engine);
+var name = "level1";
+
+function sceneData() {
+    // Configurez une caméra
+    var camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0, 5, -10), scene);
+    camera.setTarget(BABYLON.Vector3.Zero());
+
+    // Ajoutez une lumière
+    var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+
+    // Créez un matériau pour le cube (bleu)
+    var material = new BABYLON.StandardMaterial("blueMaterial", scene);
+    material.diffuseColor = new BABYLON.Color3(1, 0, 0); // Couleur rouge
+
+
+    // Créez un cube avec le matériau
+    var cube = BABYLON.MeshBuilder.CreateBox("blueCube", { size: 2 }, scene);
+    cube.material = material;
+
+    // Positionnez le cube où vous le souhaitez
+    cube.position = new BABYLON.Vector3(0, 1, 0);
+}
+
+function launch() {
+    sceneData();
+    engine.runRenderLoop(function () {
+        scene.render();
+    });
+}
+
+function getScene() {
+    return scene;
+}
+
+export { name, scene, sceneData, launch };
