77425a65ef14f56504018b3bbab7feae8ad12a12
Nayarjay <74157421+Nayarjay@users.noreply.github.com>
2023-10-23 11:42:50 +0200

    quick fix

--- game/PlayerCamera.js
+++ game/PlayerCamera.js
@@ -43,3 +43,4 @@ class PlayerCamera {
         });
     }
 }
+export default PlayerCamera;
--- game/scene2.js
+++ game/scene2.js
@@ -15,11 +15,6 @@ function sceneData() {
     var camera = new DevCamera(canvas, scene);
 
  
-    /*
-    // Configurez une caméra
-    var camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0, 5, -10), scene);
-    camera.setTarget(BABYLON.Vector3.Zero());
-    camera.attachControl();*/
 
     // Ajoutez une lumière
     var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
