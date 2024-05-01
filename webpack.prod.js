1942aeef297361996474982b084453685abd93c2
Nayarjay <74157421+Nayarjay@users.noreply.github.com>
2024-05-01 14:36:11 +0200

    add plugins

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,18 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = merge(common, {
-mode: 'production',
-devtool: 'source-map'
+  mode: 'production',
+  devtool: 'source-map',
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'models', // Dossier source des modèles 3D
+          to: 'models' // Dossier de destination dans le dossier de sortie final
+        }
+      ]
+    })
+  ]
 });
